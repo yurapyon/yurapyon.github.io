f96e07f42a2a6150debf9dc5b82ab1338a6ee727
yurapyon <genericpb@gmail.com>
2023-01-17 21:22:38 -0600

    www

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pyon.moe",
+  site: "https://www.pyon.moe",
   integrations: [tailwind()]
 });
