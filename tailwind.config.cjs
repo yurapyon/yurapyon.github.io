b26355ff62e6dfd9431fd251aca69c4191913ffa
yurapyon <genericpb@gmail.com>
2023-07-15 21:00:10 -0500

    qwertyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'lmn-slate': '#161616',
+				'lmn-blue': '#34a3e0',
+				'lmn-pink': '#f081b7',
+				'lmn-yellow': '#fedc3e',
+				'lmn-green': '#4ec67c',
+				'lmn-blood': '#197cb3',
+				'lmn-red': '#c5180d',
+			},
+		},
 	},
 	plugins: [],
 }
