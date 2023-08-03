78f8d8c433a2b27238f4365494bc9a0b2e774aad
yurapyon <genericpb@gmail.com>
2023-08-03 01:58:30 -0500

    asdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.pyon.moe",
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), mdx()]
 });
